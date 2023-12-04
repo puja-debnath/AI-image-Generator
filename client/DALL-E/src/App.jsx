90d4bc822873c931011bb9a9d3c74db204d2f5f5
unknown <pujadebnath570@gmail.com>
2023-12-05 00:53:14 +0530

    frontend

--- client/DALL-E/src/App.jsx
+++ client/DALL-E/src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
+import { logo } from './assets';
+import { Home, CreatePost } from './pages';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => (
+  <BrowserRouter>
+   <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4  border-b border-b-yellow-50 '>
+    <Link to="/" >
+      <img  src={logo}  alt="logo"  className='w-28 object-contain' />
+    </Link>
 
-  return (
-   <h1>jshfdjhdfg</h1>
-  )
-}
+    <Link to="/Create-Post" className='bg-blue-400 px-4 py-2 text-white font-inter font-medium rounded-md'>Create
+    </Link>
+   </header>
+   <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+       <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Create-Post' element={<CreatePost/>} />
 
-export default App
+       </Routes>
+   </main>
+  </BrowserRouter>
+);
+
+export default App;
